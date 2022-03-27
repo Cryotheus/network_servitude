5c58d54fb960e1c7e2e833850a397d5a427e3efb
Silly Goose <sillygoose078@gmail.com>
2022-03-27 07:06:17 -0400

    Recipe Changes
    Recipes Changed:
    appliedenergistics2:network/parts/quartz_fiber_part
    appliedenergistics2:appliedenergistics2:network/cables/smart_fluix_clean
    appliedenergistics2:network/cables/dense_smart_fluix_clean

--- overrides/kubejs/server_scripts/stryker/recipes/appliedenergistics2.js
+++ overrides/kubejs/server_scripts/stryker/recipes/appliedenergistics2.js
@@ -1,4 +1,24 @@
 onEvent('recipes', (event) => {
+    event.remove({id: 'appliedenergistics2:grinder/certus_quartz_dust_ore'})
+	event.remove({id: 'appliedenergistics2:grinder/nether_quartz_dust_ore'})
+	event.remove({id: 'appliedenergistics2:grinder/gold_dust_ore'})
+	event.remove({id: 'appliedenergistics2:grinder/iron_dust_ore'})
+    event.remove({id: 'appliedenergistics2:network/parts/quartz_fiber_part'})
+    event.remove({id: 'appliedenergistics2:network/cables/smart_fluix_clean'})
+    event.remove({id: 'appliedenergistics2:network/cables/dense_smart_fluix_clean'})
+
+    event.shaped('4x appliedenergistics2:quartz_fiber', [
+        ' G ',
+        'GQG',
+        ' G '
+    ], {
+        G: '#forge:glass_panes',
+        Q: '#appliedenergistics2:dusts/quartz'
+    })
+
+    event.shapeless('appliedenergistics2:fluix_smart_cable', ['#appliedenergistics2:smart_cable'])
+    event.shapeless('appliedenergistics2:fluix_smart_dense_cable', ['#appliedenergistics2:smart_dense_cable'])
+
     const ore_chunks = Ingredient.of('#forge:chunks').itemIds;
     ore_chunks.forEach((value) => {
         materialTag = value.substring(0,value.length-5)+'dust'
