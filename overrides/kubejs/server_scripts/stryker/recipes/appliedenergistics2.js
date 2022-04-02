bc896b951f44f0cd1794858500a5d0ddcc822770
Cryotheus <14439112+Cryotheus@users.noreply.github.com>
2022-04-02 07:30:57 -0600

    Time in a Bottle and more
    Improved quests for AE2 Spatial Storage, removed glitched quest, and cleaned up an internal quest.

--- overrides/kubejs/server_scripts/stryker/recipes/appliedenergistics2.js
+++ overrides/kubejs/server_scripts/stryker/recipes/appliedenergistics2.js
@@ -1,17 +1,8 @@
-onEvent('recipes', (event) => {
-    //grinder recipes
-    for(mat of global.retags){
-        if(Ingredient.of('emendatusenigmatica:'+mat+'_dust').isInvalidRecipeIngredient()){
-            continue
-        }else{
-            event.remove({type: 'appliedenergistics2:grinder', input: '#forge:ores/'+mat})
-            generateOreChunkGrindingRecipes(event, mat)
-        }
-    }
-
-    createAdvancedGrindingRecipe(event, "forge:chunks/charged_certus_quartz", "forge:dusts/certus_quartz", 2, "forge:dusts/certus_quartz", 0.9, 1, 4)
-    createAdvancedGrindingRecipe(event, "forge:chunks/bitumen", "forge:bitumen", 1, "forge:bitumen", 0.9, 1, 4)
-})
+function add_quest_tag(event, item, quest) {
+    event.add('kubejs:stryker_quests', item)
+    
+    if (quest) {event.add('kubejs:stryker_quests/' + quest, item)}
+}
 
 function generateOreChunkGrindingRecipes(event, material){
     event.custom({
@@ -58,3 +49,23 @@ function createAdvancedGrindingRecipe(event, input, result, resultCount, extraRe
     })
 }
 
+onEvent('item.tags', event => {
+    add_quest_tag(event, 'appliedenergistics2:2_cubed_spatial_storage_cell', 'spatial_cell')
+    add_quest_tag(event, 'appliedenergistics2:16_cubed_spatial_storage_cell', 'spatial_cell')
+    add_quest_tag(event, 'appliedenergistics2:128_cubed_spatial_storage_cell', 'spatial_cell')
+})
+
+onEvent('recipes', event => {
+    //grinder recipes
+    for(mat of global.retags){
+        if(Ingredient.of('emendatusenigmatica:'+mat+'_dust').isInvalidRecipeIngredient()){
+            continue
+        }else{
+            event.remove({type: 'appliedenergistics2:grinder', input: '#forge:ores/'+mat})
+            generateOreChunkGrindingRecipes(event, mat)
+        }
+    }
+
+    createAdvancedGrindingRecipe(event, "forge:chunks/charged_certus_quartz", "forge:dusts/certus_quartz", 2, "forge:dusts/certus_quartz", 0.9, 1, 4)
+    createAdvancedGrindingRecipe(event, "forge:chunks/bitumen", "forge:bitumen", 1, "forge:bitumen", 0.9, 1, 4)
+})
