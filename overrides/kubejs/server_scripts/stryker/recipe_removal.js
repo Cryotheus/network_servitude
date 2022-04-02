423c7b496829cffda0b20d60d2a10da5e604d3ab
Cryotheus <14439112+Cryotheus@users.noreply.github.com>
2022-04-01 18:12:57 -0600

    More Recipes and Quest Chapters

--- overrides/kubejs/server_scripts/stryker/recipe_removal.js
+++ overrides/kubejs/server_scripts/stryker/recipe_removal.js
@@ -1,4 +1,11 @@
 onEvent('recipes', event => {
-	//event.remove({id: 'botania:orechid'})
+	//we have environmental tech for a reason
+	event.remove({id: 'botania:petal_apothecary/orechid'})
+	event.remove({id: 'botania:petal_apothecary/orechid_ignem'})
+	
+	//conflicting recipe
 	event.remove({id: 'extendedcrafting:handheld_table'})
+	
+	//because we allow all gems
+	event.remove({id: 'blue_skies:enchanting_table_compat'})
 })
