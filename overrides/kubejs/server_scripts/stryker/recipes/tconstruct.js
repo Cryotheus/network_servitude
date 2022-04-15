5858342d6393f611ba2048264461d414ed887a7b
Cryotheus <14439112+Cryotheus@users.noreply.github.com>
2022-04-15 14:31:44 -0400

    Updated mod list
    Also cleaned up the tconstruct.js kubejs script. I'm leaving the casting recipe script there because it may be useful in the future.

--- overrides/kubejs/server_scripts/stryker/recipes/tconstruct.js
+++ overrides/kubejs/server_scripts/stryker/recipes/tconstruct.js
@@ -1,24 +1,22 @@
+function addCastingBasinRecipeFromItem(event, cast, cast_consumed, fluid, volume, result, cooling_time){
+	event.custom({		
+		type: "tconstruct:casting_basin",
+		cast: {item: cast},
+		cast_consumed: cast_consumed,
+		cooling_time: cooling_time,
+		fluid: {
+			tag: fluid,
+			amount: volume
+		},
+		result: result,	  
+	})
+}
+
 onEvent("recipes", event => {
 	//additions
-	//casting_basin
-
 	//shaped
 	event.shaped("tconstruct:crafting_station", ["P", "C"], {
 		C: "#forge:workbench",
 		P: "tconstruct:pattern"
 	})
 })
-
-function addCastingBasinRecipeFromItem(event, cast, fluid, fluidAmount, result, cooling_time){
-	event.custom({		
-		type: "tconstruct:casting_basin",
-		cast: {item: cast},
-		cast_consumed: true,
-		fluid: {
-			tag: "tconstruct:molten_clay",
-			amount: 500
-		},
-		result: "tconstruct:seared_fancy_bricks",
-		cooling_time: 90		  
-	})
-}
